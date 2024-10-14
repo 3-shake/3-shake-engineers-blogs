a13e8edf4b96a47fcb200bcfd3eb9a0c82ae7002
Shuya Motouchi <syu.m.5151@gmail.com>
2024-10-15 01:05:13 +0900

    Update ScrollableMembers.tsx

--- src/components/ScrollableMembers.tsx
+++ src/components/ScrollableMembers.tsx
@@ -1,11 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import Link from "next/link";
 import { members } from "@members";
 import { getMemberPath } from "@src/utils/helper";
 
 export const ScrollableMembers: React.FC = () => {
+  const [randomMembers, setRandomMembers] = useState(members);
+
+  useEffect(() => {
+    const shuffleMembers = () => {
+      const shuffled = [...members].sort(() => 0.5 - Math.random());
+      setRandomMembers(shuffled.slice(0, 8)); // Display 8 random members
+    };
+
+    shuffleMembers();
+    const interval = setInterval(shuffleMembers, 60000); // Reshuffle every minute
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="scrollable-members">
-      {members.map((member, i) => (
+      {randomMembers.map((member, i) => (
         <Link
           key={`scrollable-member-${i}`}
           href={getMemberPath(member.id)}
