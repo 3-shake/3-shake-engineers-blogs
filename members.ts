a58b03b8f226087ef038c1fb50bc2b734f59478b
atusy <30277794+atusy@users.noreply.github.com>
2024-11-05 10:00:06 +0900

    feat(members): add atusy

--- members.ts
+++ members.ts
@@ -519,4 +519,16 @@ export const members: Member[] = [
     githubUsername: "melanmeg",
     websiteUrl: "https://lit.link/melanmeg",
   },
+  {
+    id: "atusy",
+    name: "Atsushi Yasumoto",
+    role: "Software Developer",
+    bio: "loves programming",
+    avatarSrc: "/avatars/atusy.jpg",
+    sources: ["https://blog.atusy.net/index.xml"],
+    includeUrlRegex: "",
+    twitterUsername: "Atsushi776",
+    githubUsername: "atusy",
+    websiteUrl: "https://blog.atusy.net/",
+  },
 ].sort((a, b) => (a.id < b.id ? -1 : 1));
